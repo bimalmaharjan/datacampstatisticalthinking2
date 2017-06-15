d5039279f854671ad5c419e56b82dfd62d0b581b
bimal maharjan <hakubimal@gmail.com>
2017-06-15 11:35:21 -0400

    finding minimum

--- linearregression.py
+++ linearregression.py
@@ -94,9 +94,11 @@ a_vals = np.linspace(0,0.1,200)
 # Initialize sum of square of residuals: rss
 rss = np.empty_like(a_vals)
 
+
+
 # Compute sum of square of residuals for each value of a_vals
 for i, a in enumerate(a_vals):
-    rss[i] = np.sum(((fertility - a) *(illiteracy - b))**2)
+    rss[i] = np.sum((np.array(fertility)- (a* np.array(illiteracy)) - b)**2)
 
 # Plot the RSS
 plt.plot(a_vals,rss , '-')
