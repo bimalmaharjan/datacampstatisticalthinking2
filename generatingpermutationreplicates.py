4bb688f78277061d233ad1e3d67a3582b02e076e
bimal maharjan <hakubimal@gmail.com>
2017-06-15 14:44:02 -0400

    added p value on frog data

---
+++ generatingpermutationreplicates.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from visualizingpermutationsampling import permutation_sample
+
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+
+    # Initialize array of replicates: perm_replicates
+    perm_replicates = np.empty(size)
+
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1,data_2)
+
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1,perm_sample_2)
+
+    return perm_replicates
+
+index = np.array([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+            37, 38, 39, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73,
+            74, 75, 76, 77, 78, 79])
+ID = np.array(['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+       'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B',
+       'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'])
+impact_force = np.array([ 1.612,  0.605,  0.327,  0.946,  0.541,  1.539,  0.529,  0.628,
+        1.453,  0.297,  0.703,  0.269,  0.751,  0.245,  1.182,  0.515,
+        0.435,  0.383,  0.457,  0.73 ,  0.172,  0.142,  0.037,  0.453,
+        0.355,  0.022,  0.502,  0.273,  0.72 ,  0.582,  0.198,  0.198,
+        0.597,  0.516,  0.815,  0.402,  0.605,  0.711,  0.614,  0.468])
+
+
+df_ID = pd.DataFrame((ID), index=index, columns=['ID'])
+df_impact_force = pd.DataFrame((impact_force),index=index, columns=['impact_force'])
+df = df_ID.join(df_impact_force)
+
+
+# Make bee swarm plot
+_ = sns.swarmplot(x= 'ID', y= 'impact_force', data= df)
+
+# Label axes
+_ = plt.xlabel('frog')
+_ = plt.ylabel('impact force (N)')
+
+# Show the plot
+plt.show()
+
+
+force_a = np.array( [1.612,  0.605,  0.327,  0.946,  0.541,  1.539,  0.529,  0.628,
+        1.453,  0.297,  0.703,  0.269,  0.751,  0.245,  1.182,  0.515,
+        0.435,  0.383,  0.457,  0.73 ])
+
+force_b = np.array([ 0.172,  0.142,  0.037,  0.453,  0.355,  0.022,  0.502,  0.273,
+        0.72 ,  0.582,  0.198,  0.198,  0.597,  0.516,  0.815,  0.402,
+        0.605,  0.711,  0.614,  0.468])
+
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+# Compute difference of mean impact force from experiment: empirical_diff_means
+empirical_diff_means = diff_of_means(force_a,force_b)
+
+# Draw 10,000 permutation replicates: perm_replicates
+perm_replicates = draw_perm_reps(force_a, force_b,
+                                 diff_of_means, size=10000)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= empirical_diff_means) / float(len(perm_replicates))
+
+# Print the result
+print('p-value =', p)
+
