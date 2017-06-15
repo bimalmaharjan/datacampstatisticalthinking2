f0de38afa0c59a9280de51829057583aa1cf98f0
bimal maharjan <hakubimal@gmail.com>
2017-06-15 12:16:04 -0400

    added ascombe and bootstrap

---
+++ LinearregressiononappropriateAnscombedata.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import sys
+
+
+x = [ 10.,   8.,  13.,   9.,  11.,  14.,   6.,   4.,  12.,   7.,   5.]
+y = [  8.04,   6.95,   7.58,   8.81,   8.33,   9.96,   7.24,   4.26,
+        10.84,   4.82,   5.68]
+
+# Perform linear regression: a, b
+a, b = np.polyfit(x,y,1)
+
+# Print the slope and intercept
+print(a, b)
+
+# Generate theoretical x and y data: x_theor, y_theor
+x_theor = np.array([3, 15])
+y_theor = a * x_theor + b
+
+# Plot the Anscombe data and theoretical line
+_ = plt.plot(x,y, marker ='.', linestyle ='none')
+_ = plt.plot(x_theor,y_theor, marker ='.', linestyle ='none')
+
+# Label the axes
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Show the plot
+plt.show()
